ec3ef3aa9817528f7548b8654292b9eb24ad7e9a
Пономарева Анастасия Андреевна <ponomareva.aa@reliab.tech>
2022-08-31 18:10:07 +0300

    задача 1

--- src/project.java
+++ src/project.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class project {
-    public static void main(String args[])
-    {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        System.out.println("Введите три числа через Enter");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int third = scanner.nextInt();
+
+        if (first == second || first == third){
+            System.out.print(first + " ");
+        }
+
+        if (second == first || second == third){
+            System.out.print(second + " ");
+        }
+
+        if (third == first || third == second){
+            System.out.print(third);
+        }
+
+        scanner.close();
     }
 }
+
